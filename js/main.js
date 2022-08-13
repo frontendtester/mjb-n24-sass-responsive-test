c10a008b7b9dfe29fd3c595ac371c148ec795227
frontendtester <frontendtester@yandex.ru>
2022-08-13 12:53:41 +0500

    Make site-header responsive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 const elModal = document.querySelector('.modal');
 const elModalClose = document.querySelector('.js-modal-close');
 
+const elSiteHeader = document.querySelector('.site-header');
+const elSiteHeaderToggler = elSiteHeader.querySelector('.js-site-header-toggler');
+
+if (elSiteHeaderToggler) {
+  elSiteHeaderToggler.addEventListener('click', function () {
+    elSiteHeader.classList.toggle('site-header-open');
+  });
+}
+
 if (elModalClose) {
   elModalClose.addEventListener('click', function () {
     elModal.classList.remove('modal-open');
